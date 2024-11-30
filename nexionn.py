30db5a0ce831ff4841bd45cc3a794dcf58fd46cd
Money8989 <thakur.adarsh9119@gmail.com>
2024-11-30 19:00:01 +0530

    Update nexionn.py

--- nexionn.py
+++ nexionn.py
@@ -1,7 +1,7 @@
 # nexionn.py
 
 # Bot token from Telegram
-TOKEN = "7099053793:AAGcbJDw99PJntP5sZNioWfUYZusyJ-Q8uQ"
+TOKEN = "7604576652:AAFJ7YjLiIbHJCTJwhO-9Tr3PtMOazcjRwE"
 
 # Admin ID (Telegram user ID)
-ADMIN_ID = 1847934841
+ADMIN_ID = 6049870925
